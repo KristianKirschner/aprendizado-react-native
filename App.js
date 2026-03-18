ed23b9f6283d011b92c66a41982d131cbe19f463
unknown <kristianlll@hotmail.com>
2026-03-18 14:55:55 -0300

    Aplicativo cronometro

--- App.js
+++ App.js
@@ -1,48 +1,73 @@
 import React, { Component } from "react";
-import { View, StyleSheet, Image, Text, TouchableOpacity} from "react-native";
+import { View, StyleSheet, Image, Text, TouchableOpacity } from "react-native";
 
 class App extends Component {
-  constructor(props){
+
+  constructor(props) {
     super(props);
     this.state = {
-      textoFrase: '',
-      img: require('./src/biscoito.png')
-    }
+      numero: 0,
+      botao: 'VAI',
+      ultimo: null
+    };
 
-    this.frases = [
-      'Siga os bons e aprenda com eles.', 
-      'O bom-senso vale mais do que muito conhecimento.', 
-      'O riso é a menor distância entre duas pessoas.', 
-      'Deixe de lado as preocupações e seja feliz.',
-      'Realize o óbvio, pense no improvável e conquiste o impossível.',
-      'Acredite em milagres, mas não dependa deles.',
-      'A maior barreira para o sucesso é o medo do fracasso.'
-    ];
+    this.timer = null;
+    this.vai = this.vai.bind(this);
+    this.limpar = this.limpar.bind(this);
 
-    this.quebraBiscoito = this.quebraBiscoito.bind(this)
   }
 
-  quebraBiscoito(){
-    let numeroAleatorio = Math.floor(Math.random() * this.frases.length )
-    this.setState({
-      textoFrase:  '"' + this.frases[numeroAleatorio] + '"',
-      img: require('./src/biscoitoAberto.png')
-    })
+  vai(){
+
+    if(this.timer != null){
+      clearInterval(this.timer);
+      this.timer = null;
+      this.setState({botao: 'VAI'})
+    } else {
+      this.timer = setInterval(() => {
+        this.setState({numero: this.state.numero + 0.01})
+      }, 1);
+      this.setState({botao: 'PAUSAR'})
+    }
+
+
+  }
+
+  limpar(){
+
+    if (this.timer != null){
+      clearInterval(this.timer);
+      this.timer = null;
+    }
+    if (this.state.numero != 0){
+    this.setState({ultimo: this.state.numero.toFixed(2), numero: 0, botao: 'VAI'})
+    }
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Image
-        style={styles.img}
-        source={this.state.img}
-        />
-      <Text style={styles.textofrase}>{this.state.textoFrase}</Text>
-      <TouchableOpacity onPress={this.quebraBiscoito} style={styles.botao}>
-        <View style={styles.btnarea}>
-          <Text style={styles.btntexto}>Quebrar biscoito</Text>
+        <Image source={require('./src/cronometro.png')}
+          style={styles.cronometro} />
+
+        <Text style={styles.timer} > {this.state.numero.toFixed(2)} </Text>
+
+        <View style={styles.btnArea} >
+          <TouchableOpacity style={styles.btn} 
+          onPress={this.vai}>
+            <Text style={styles.btnTexto}>{this.state.botao}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btn} 
+          onPress={this.limpar}>
+            <Text style={styles.btnTexto}>LIMPAR</Text>
+          </TouchableOpacity>
+
+
         </View>
-      </TouchableOpacity>
+
+          <View style={styles.areaUltimo} >
+            <Text style={styles.textoCorrida} > Ultimo Tempo : {this.state.ultimo} </Text>
+          </View>
 
       </View>
     );
@@ -50,40 +75,46 @@ class App extends Component {
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center' 
-  },
-  img: {
-    width: 200,
-    height: 200
-  },
-  textofrase: {
-    fontSize: 20,
-    color: '#dd7b22',
-    margin: 30,
-    fontStyle: 'italic',
-    textAlign: 'center'
-  },
-  botao: {
-    width: 230,
-    height: 50,
-    borderWidth: 2,
-    borderColor: '#dd7b22',
-    borderRadius: 25
-  },
-  btnarea: {
-    flex: 1,
-    flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'center'
+    backgroundColor: '#00aeef',
   },
-  btntexto: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#dd7b22'
+  timer: {
+    marginTop: -160,
+    color: 'white',
+    fontSize: 65,
+    fontWeight: 'bold'
+  },
+  btnArea: {
+    flexDirection: 'row',
+    marginTop: 75,
+    height: 40
+  },
+  btn: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    height: 40,
+    margin: 17,
+    borderRadius: 9
+  },
+  btnTexto: {
+    fontSize: 20,
+    color: '#00aeef',
+    fontWeight: 'bold'
+  },
+  areaUltimo: {
+    marginTop: 50,
+  },
+  textoCorrida: {
+    fontSize: 25,
+    fontStyle: 'italic',
+    color: 'white'
   }
+
 })
 
 export default App;
