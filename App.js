1d9184d3b0518674d2b98a04cb4835c188a07d92
unknown <kristianlll@hotmail.com>
2026-03-21 12:49:05 -0300

    Componente: Switch

--- App.js
+++ App.js
@@ -1,59 +1,27 @@
-import Slider from "@react-native-community/slider";
-import { Picker } from "@react-native-picker/picker";
 import React, { Component } from "react";
-import {View, StyleSheet, Text } from 'react-native'
+import {View, StyleSheet, Text, Switch } from 'react-native'
 
 export default class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      pizza: 0,
-      pizzas: [
-        {key: 1, nome: 'Strogonoff', valor: 35.90},
-        {key: 2, nome: 'Calabresa', valor: 59.90},
-        {key: 3, nome: 'Quatro Queijos', valor: 35.90},
-        {key: 4, nome: 'Brigadeiro', valor: 45.90},
-        {key: 5, nome: 'Marguerita', valor: 40.90},        
-      ],
-      valor: 50
+      status: false
     }
   }
 
 
   render(){
 
-    let pizzasItem = this.state.pizzas.map((v, k) => {
-      return <Picker.Item  key={k} value={k} label={v.nome} />
-    }) 
-
     return(
       <View style={styles.container} >
-
-        <Text style={styles.logo} >Menu Pizza</Text>
-
-        <Picker 
-          selectedValue={this.state.pizza}
-          onValueChange={(itemValue, itemIndex) => this.setState({pizza: itemValue})}
-        >
-        {
-          pizzasItem
-        }
-
-        </Picker>
-
-        <Text style={styles.pizza} >Você escolheu: {this.state.pizzas[this.state.pizza].nome} </Text>
-        <Text style={styles.pizza} >R$ {this.state.pizzas[this.state.pizza].valor.toFixed(2).replace('.', ',')} </Text>
-
-        <Slider 
-          minimumValue={0}
-          maximumValue={100}
-          onValueChange={(valorSelecionado) => this.setState({ valor: valorSelecionado})}
-          value={this.state.valor}
-          minimumTrackTintColor="#00FF00"
-          maximumTrackTintColor="#ff0000"          
+        <Switch 
+          value={this.state.status}
+          onValueChange={(valorSelecionado) => this.setState({status: valorSelecionado}) }
+          thumbColor='black'
         />
-
-        <Text style={styles.sliderValor} > {this.state.valor.toFixed(2)} </Text>
+        <Text style={{textAlign: 'center', fontSize: 40}} >
+          {(this.state.status) ? "True" : "False"}
+        </Text>
 
       </View>
     )
@@ -64,21 +32,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center'
-  },
-  logo: {
-    textAlign: 'center',
-    fontSize: 30,
-    fontWeight: 'bold'
-  },
-  pizza: {
-    textAlign: 'center',
-    fontSize: 25
-  },
-  sliderValor: {
-    fontSize: 25,
-    textAlign: 'center'
   }
-
-
-
 });
