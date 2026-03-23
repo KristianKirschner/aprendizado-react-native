335522600af235230aefca2d6037db2f3539be63
Kristian <kristianlll@hotmail.com>
2026-03-23 20:36:31 -0300

    APIs useState e useEffect

--- App.js
+++ App.js
@@ -1,115 +1,68 @@
-import React, { Component } from "react";
-import { View, StyleSheet, Text, Button, Modal, Image, TextInput, TouchableOpacity } from "react-native"
-import Calculo from './src/components/Calculo'
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, TouchableOpacity, TextInput} from "react-native"
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export default class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      valorgasolina: 7.3,
-      valoralcool: 4.6,
-      recomendacao: '',
-      modalVisible: false
-    }
-    this.sair = this.sair.bind(this)
-    this.calculo = this.calculo.bind(this)
-  }
+export default function App () {
 
-  sair(status){
-    this.setState({
-      modalVisible: status
-    })
-  }
+  const [nome, setNome] = useState("")
+  const [input, setInput] = useState('')
 
-  calculo(){
-    let divisao = this.state.valoralcool / this.state.valorgasolina
-    if (divisao < 0.7) {
-      this.setState({
-        recomendacao: 'Álcool'
-      })
-    } else{
-      this.setState({
-        recomendacao: 'Gasolina'
-      })
-    }
-  }
+    // ComponenteDidMount
+    useEffect(()=>{
+        async function getStorage() {
+          const nomeStorage = await AsyncStorage.getItem("nomes");
+            if (nomeStorage !== null) {
+              setNome(nomeStorage);
+            }
+          }
 
-  render() {
+        getStorage();
+    }, [])
+
+    // ComponentDidUpdate
+    useEffect(()=>{
+      async function saveStorage() {
+        await AsyncStorage.setItem('nomes', nome)
+      }
+
+      saveStorage();
+    }, [nome])
+
     return (
       <View style={styles.container} >
-        <Image source={require('./src/images/logo.png')} />
-        <Text style={styles.textLogo} >Qual melhor opção?</Text>
 
-        <View style={styles.inputs} >
-          <Text style={styles.text} >Álcool (preço por litro)</Text>
-          <TextInput onChangeText={(valor) => {this.setState({valoralcool: valor})}} style={styles.inputText} />      
-          <Text style={styles.text}>Gasolina (preço por litro)</Text>
-          <TextInput onChangeText={(valor) => {this.setState({valorgasolina: valor})}} style={styles.inputText} />
-          <TouchableOpacity onPress={() => this.setState({modalVisible: true}, this.calculo())} style={styles.btn} >
-            <Text style={styles.btnText} >Calcular</Text>
-          </TouchableOpacity>
-        </View>
+        <Text style={styles.texto} > {nome} </Text>
 
-        <Modal transparent={true} animationType="slide" visible={this.state.modalVisible}>
-          <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-              <Calculo valorgasolina={this.state.valorgasolina} valoralcool={this.state.valoralcool} decisao={this.state.recomendacao} fechar={ () => this.sair(false)  } />
-          </View>
-        </Modal>
+        <TextInput
+          value={input}
+          placeholder="digite o nome"
+          onChangeText={(texto) => {setInput(texto)}}
+        />
+
+        <TouchableOpacity onPress={()=> 
+          {setNome(input) 
+          setInput('')}} >
+          <Text>Altera text</Text>
+        </TouchableOpacity>
+
+
+
+
 
       </View>
     )
-  }
+  
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#212121',
-    justifyContent: 'center',
-    alignItems: 'center'
+    marginTop: 30
   },
-  textLogo: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    color: '#FFFF',
-    marginTop: 20
-  },
-  inputs: {
-    marginTop: 60,
-    width: '90%',
-  },
-  text: {
-    color: 'white',
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 5
-  },
-  inputText: {
-    height: 35,
-    backgroundColor: 'white',
-    marginBottom: 15,
-    borderRadius: 5,
-    padding: 0,
-    paddingLeft: 15,
-    alignItems: 'center',
-    fontSize: 20,
-    color: 'black'
-  },
-  btnText: {
-    color: 'white',
-    fontWeight: 'bold',
-    fontSize: 23,
-  },
-  btn: {
-    width: '100%',
-    backgroundColor: '#FF512E',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 40,
-    borderRadius: 5,
-    marginTop: 10
+  texto: {
+    color: '#000',
+    fontSize: 35
   }
 
 
-}
-);
+})
