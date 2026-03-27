6fd881c25bb63f5491dbdd1c687fc46f6435462e
unknown <kristianlll@hotmail.com>
2026-03-27 14:48:01 -0300

    Animações

--- App.js
+++ App.js
@@ -1,66 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { View, StyleSheet, Text, Animated } from 'react-native';
 
 export default function App() {
-  const [pessoas, setPessoas] = useState(10);
-  const [limiteSup, setLimiteSup] = useState(false);
-  const [limiteInf, setLimiteInf] = useState(false);
 
-  useEffect(() => {
-    function checarNumeros() {
-      if (pessoas >= 10) {
-        setLimiteSup(true);
-      } else {
-        setLimiteSup(false)
-      };
-      if (pessoas <= 0) {
-        setLimiteInf(true)
-      } else {
-        setLimiteInf(false)
-      }
-    }
-    checarNumeros();
-  }, [pessoas]);
+    const larAnimado =useRef (new Animated.Value(50)).current
+  
 
-  function renderizarLimite(){
-    if (limiteSup) {
-      return(
-        <Text style={{backgroundColor: 'yellow'}} >Restaurante está no seu limite de pessoas</Text>
-      )
-    }
-  }
+useEffect(()=>{
+
+  Animated.timing(larAnimado, {
+    toValue: 500,
+    duration: 2000,
+    useNativeDriver: false
+  }).start();
+
+}, []);
+
 
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 25, marginBottom: 10 }}>
-        Pessoas no restaurante
-      </Text>
-
-      <View style={styles.caixaNumero}>
-        <Text style={{ color: 'white', fontSize: 40 }}> {pessoas} </Text>
-      </View>
-
-      {limiteSup &&
-        <Text style={{margin: 10, padding: 2, backgroundColor: '#F6B136'}} >Restaurante está no limite de pessoas</Text>
-      }
-
-      <View style={styles.caixaInput}>
-        <TouchableOpacity
-          style={styles.botao}
-          onPress={() => setPessoas(pessoas + 1)}
-          disabled={limiteSup}
-        >
-          <Text style={{ color: 'white' }}>Adicionar</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => setPessoas(pessoas - 1)}
-          style={styles.botao}
-          disabled={limiteInf}
-        >
-          <Text style={{ color: 'white' }}>Remover</Text>
-        </TouchableOpacity>
-    
-      </View>
+      <Animated.View 
+      style={{
+        width: 300,
+        height: larAnimado,
+        backgroundColor: '#4169e1',
+        justifyContent: 'center'
+      }} >
+        <Text style={{textAlign: 'center', fontSize: 22, color: '#FFFF'}} >Carregando...</Text>
+      </Animated.View >
 
     </View>
   );
@@ -69,30 +36,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FAFAFA',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  caixaNumero: {
-    fontSize: 30,
-    backgroundColor: 'black',
-    width: 90,
-    height: 90,
-    textAlign: 'center',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 8,
-    textAlign: 'centers',
-  },
-  caixaInput: {
-    flexDirection: 'row',
-  },
-  botao: {
-    backgroundColor: '#007AFE',
-    borderRadius: 8,
-    width: 80,
-    height: 35,
-    margin: 20,
     alignItems: 'center',
     justifyContent: 'center',
   },
