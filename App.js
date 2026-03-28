204f71aa0e20de3a1630611908009c52e88688f1
unknown <kristianlll@hotmail.com>
2026-03-28 07:15:02 -0300

    Interpolação com animações

--- App.js
+++ App.js
@@ -2,33 +2,48 @@ import React, { useEffect, useRef } from 'react';
 import { View, StyleSheet, Text, Animated } from 'react-native';
 
 export default function App() {
+  const larAnimado = useRef(new Animated.Value(0)).current;
+  const altAnimado = useRef(new Animated.Value(50)).current;
 
-    const larAnimado =useRef (new Animated.Value(50)).current
-  
+  useEffect(() => {
+    Animated.sequence([    
+      Animated.timing(larAnimado, {
+      toValue: 100,
+      duration: 1000,
+      useNativeDriver: false,
+    }),
+    Animated.timing(altAnimado, {
+      toValue: 100,
+      duration: 1000,
+      useNativeDriver: false
+    })
+  ]).start(()=>{
+    alert('Animacao finalizada')
+  })
 
-useEffect(()=>{
+  }, []);
 
-  Animated.timing(larAnimado, {
-    toValue: 500,
-    duration: 2000,
-    useNativeDriver: false
-  }).start();
+  let porcentagemLargura = larAnimado.interpolate({
+    inputRange: [0, 100],
+    outputRange: ['0%', '100%']
+  })
 
-}, []);
-
+  let porcentagemAltura = altAnimado.interpolate({
+    inputRange: [50, 100],
+    outputRange: ['5%', '100%']
+  })
 
   return (
     <View style={styles.container}>
-      <Animated.View 
-      style={{
-        width: 300,
-        height: larAnimado,
-        backgroundColor: '#4169e1',
-        justifyContent: 'center'
-      }} >
-        <Text style={{textAlign: 'center', fontSize: 22, color: '#FFFF'}} >Carregando...</Text>
-      </Animated.View >
-
+      <Animated.View
+        style={{
+          width: porcentagemLargura,
+          height: porcentagemAltura,
+          backgroundColor: '#4169e1',
+          justifyContent: 'center',
+        }}
+      >
+      </Animated.View>
     </View>
   );
 }
