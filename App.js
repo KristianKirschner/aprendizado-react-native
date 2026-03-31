00bb24c05340076f80241198e762d975ed16f740
Kristian <kristianlll@hotmail.com>
2026-03-31 20:23:22 -0300

    Cadastrando no banco de dados

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, TextInput } from 'react-native';
 import { db } from './src/firebaseConnection';
-import { doc, getDoc, onSnapshot } from 'firebase/firestore'; 
+import { doc, getDoc, onSnapshot, setDoc, collection, addDoc } from 'firebase/firestore'; 
 
 export default function App(){
 
@@ -30,10 +30,45 @@ export default function App(){
     }
     getData();
   }, [])
- 
+
+  async function handleRegistro(){
+    // setar manualmente o usuario
+/*     await setDoc(doc(db, "users", "3"), {
+      nome: "Jose",
+      idade: "30",
+      cargo: "Backend"
+    })
+    .then(()=>{
+      console.log("Cadastrado com sucesso!")
+    })
+    .catch((erro) => {
+      console.log("Erro" + erro)
+    }) */
+
+      //adicionar dinamicamente o usuario
+  await addDoc(collection(db,"users"), {
+      nome: pessoa.nome,
+      idade: pessoa.email,
+      cargo: pessoa.cargo,
+  })
+  }
+  
+  const [pessoa,setPessoa] = useState({nome: '', email: '', idade: '', cargo: ''})
+
   return(
     <View style={styles.container} >
       <Text style={{textAlign: 'center'}} >Olá {nome}!</Text>
+
+      <TextInput placeholder='Nome' value={pessoa.nome} onChangeText={(valor) => {setPessoa({...pessoa, nome: valor})}} />
+      <TextInput placeholder='Email' value={pessoa.email} onChangeText={(valor) => {setPessoa({...pessoa, email: valor})}}  />
+      <TextInput placeholder='Cargo'  value={pessoa.cargo} onChangeText={(valor) => {setPessoa({...pessoa, cargo: valor})}}  />
+
+      <TouchableOpacity style={styles.button}
+        onPress={handleRegistro}
+      >
+        <Text style={styles.buttonText} >Adicionar</Text>
+      </TouchableOpacity>
+
     </View>
   )
 }
@@ -41,5 +76,13 @@ export default function App(){
 const styles = StyleSheet.create({
   container: {
     marginTop: 50,
+  },
+  button: {
+    backgroundColor: 'black',
+    alignSelf: 'flex-start'
+  }, 
+  buttonText: {
+    padding: 8,
+    color: 'white'
   }
 })
