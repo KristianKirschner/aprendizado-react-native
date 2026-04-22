c0df981844ff663aa7f8fb93eda9773e5a6fe7b1
unknown <kristianlll@hotmail.com>
2026-04-21 23:25:39 -0300

    Configuração biblioteca de camêra e galeria

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-    ['module:react-native-dotenv', {
-      moduleName: '@env',
-      path: '.env',
-    }]
   ]
 };
