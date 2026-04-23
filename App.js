c64f3be508a7d6ec113244b17a9315825584d425
unknown <kristianlll@hotmail.com>
2026-04-22 23:26:46 -0300

    Buscando imagens da galeria

--- App.js
+++ App.js
@@ -1,9 +1,46 @@
-import { Text, View } from 'react-native';
+import { useState } from 'react';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
+import { launchImageLibrary } from 'react-native-image-picker';
 
 export default function App() {
+  const [photo, setPhoto] = useState(null);
+
+  function openAlbum() {
+    const options = {
+      mediaType: 'photo',
+      quality: 1,
+      selectionLimit: 2,
+    };
+
+    launchImageLibrary(options, respose => {
+      if (respose.didCancel) {
+        console.log('CANCELADO');
+      }
+
+      console.log(respose.assets);
+      setPhoto(respose.assets[0].uri);
+    });
+  }
+
   return (
     <View>
-      <Text>Ola</Text>
+      <TouchableOpacity>
+        <Text>Abrir camera</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={openAlbum}>
+        <Text>Abrir galeria</Text>
+      </TouchableOpacity>
+      {photo !== null && (
+        <Image
+          style={{
+            width: '90%',
+            height: 300,
+            objectFit: 'cover',
+          }}
+          source={{ uri: photo }}
+        />
+      )}
     </View>
   );
 }
